6b79ab2cc68d2c039f1e97ab9a69d15bcbe2b6db
Aaron Mussig <aaronmussig@gmail.com>
2022-06-01 10:40:38 +1000

    ci(semantic-release): Cargo build+ release, and citation version+date update.

---
+++ version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Set variables
+VERSION=$1
+RELEASE_DATE=$(date +"%Y-%m-%d")
+
+# Update Cargo version
+sed -i "s/^version = .*$/version = \"$VERSION\"/" Cargo.toml
+
+# Update citation version and date
+sed -i "s/^version: .*$/version: $VERSION/" CITATION.cff
+sed -i "s/^date-released: .*$/date-released: $RELEASE_DATE/" CITATION.cff
